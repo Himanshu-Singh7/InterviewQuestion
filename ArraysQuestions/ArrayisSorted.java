dad83d6ca8047c90adee6f4ed8521e86dd60be0e
Himanshu <himanshusingh0608@gmail.com>
2024-03-20 19:19:16 +0530

    Check the Array is Sorted or Not

---
+++ ArraysQuestions/ArrayisSorted.java
@@ -0,0 +1,25 @@
+package ArraysQuestions;
+
+import java.util.*;
+public class ArrayisSorted {
+	public static void main(String[] args) {
+     Scanner scn = new Scanner(System.in);
+     int n = scn.nextInt();
+     
+     int [] arr = new int[n];
+     for(int i = 0 ; i < arr.length ; i++) {
+    	 arr[i] =scn.nextInt();
+     }
+     boolean flag = false;
+     for(int i = 1 ; i < arr.length ; i++ ) {
+    	 if(arr[i] < arr[i -1]) {
+    		 flag  = false;
+    	 }else {
+    		  flag  = true; 
+    	 }
+    	 	 
+     }
+     System.out.println(flag);
+     
+	}
+}
